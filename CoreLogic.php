01bd02e0cc1ce275d21c60a43e3bb66c981f47c0
stetowns <stetowns@publicigroupe.net>
2023-05-31 15:41:56 +0100

    Crux Sight - Working

--- CoreLogic.php
+++ CoreLogic.php
@@ -2554,6 +2554,19 @@ function PlayAbility($cardID, $from, $resourcesPaid, $target = "-", $additionalC
     case "xWJND68I8X"://Water Barrier
       AddCurrentTurnEffect("xWJND68I8X", $currentPlayer);
       break;
+    case "P9Y1Q5cQ0F"://Crux Sight
+      if ($resourcesPaid == "2") {
+        AddDecisionQueue("MULTIZONEINDICES", $currentPlayer, "MYDISCARD:element=CRUX");
+        AddDecisionQueue("MAYCHOOSEMULTIZONE", $currentPlayer, "<-", 1);
+        AddDecisionQueue("MZADDZONE", $currentPlayer, "MYHAND", 1);
+        AddDecisionQueue("MZREMOVE", $currentPlayer, "-", 1);
+        AddDecisionQueue("DRAW", $currentPlayer, "-", 1);
+        break;
+      } else {
+          AddDecisionQueue("DRAW", $currentPlayer, "-", 1);
+          break;
+      }
+      break;
     default: break;
   }
 }
